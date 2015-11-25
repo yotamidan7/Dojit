3513502ac058764b72b617f555f55971f06b6612
yotamidan7 <yotamidan7@gmail.com>
2015-11-25 02:17:25 +0200

    Connected posts to users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
   has_many :comments
+
+  default_scope { order('created_at DESC')}
+
+  scope :ordered_by_title, -> { order('title ASC')}
+
+  scope :ordered_by_reverse_created_at, -> {order('created_at DESC')}
 end
