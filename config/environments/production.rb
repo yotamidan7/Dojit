15a77082525c0f3b950965c876a7cb56c976b2b7
yotamidan7 <yotamidan7@gmail.com>
2015-11-26 14:42:42 +0200

    Configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://Yotam-Dojit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'yotam-dojit.herokuapp.com'}
 end
